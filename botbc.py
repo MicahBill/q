26b3435d4d72094a5189511938185a395515c095
BORED <105173245+MicahBill@users.noreply.github.com>
2022-06-07 20:00:15 +0300

    Update botbc.py

--- botbc.py
+++ botbc.py
@@ -3,7 +3,7 @@
 #------------------------------------------------------------- Line: alifbudimanwahabbi
 from alipmodule import * #import module
 from linepy import *
-line = LINE('u1d356e296535337596d411d414dd8694:aWF0OiAxNjU0MTc1NTA0NDcwCg==..LC7Ru+Q7HAExQ60Xpv87DxVFwPc=')
+line = LINE('remetab867@aisezu.com', '@BdsaleH77')
 line.log("Auth Token : " + str(line.authToken))
 ALIPmid = LINE.profile.mid
 creator = ["u0be40d5b6854cce0f78b76a7ace30727"] #imput your mid here
